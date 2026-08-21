30120d4c06d52d62303b6b2ba4087e75a123c459
Andrew Pagani Lima <andrewpagani.lima0712@gmail.com>
2026-08-20 21:41:37 -0300

    Add files via upload

---
+++ Atividades 1 a 15/Media de duas notas.py
@@ -0,0 +1,7 @@
+print("== Calculadora de média ==")
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+media = (n1 + n2)/2
+print(f"A média dos seus números é {media}")
