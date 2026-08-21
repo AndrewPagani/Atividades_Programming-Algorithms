394a7a9d07c7041e29987ec8a3734cbb8bba4ed9
Andrew Pagani Lima <andrewpagani.lima0712@gmail.com>
2026-08-21 14:53:00 -0300

    Fix output formatting for average calculation

--- Atividades 1 a 15/2 - Média de duas notas.py
+++ Atividades 1 a 15/2 - Média de duas notas.py
@@ -2,4 +2,4 @@ print("== Calculadora de média ==")
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 
-print(f"A média dos seus números é {(n1 + n2)/2:.2}")
+print(f"A média dos seus números é {(n1 + n2)/2:.2f}")
