1c407dc1b9bbe5aed039e81f012b1711629a6449
Andrew Pagani Lima <andrewpagani.lima0712@gmail.com>
2026-08-20 22:00:50 -0300

    Refactor average calculator for improved readability

--- Atividades 1 a 15/2 - Média de duas notas.py
+++ Atividades 1 a 15/2 - Média de duas notas.py
@@ -1,7 +1,5 @@
 print("== Calculadora de média ==")
-
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 
-media = (n1 + n2)/2
-print(f"A média dos seus números é {media}")
+print(f"A média dos seus números é {(n1 + n2)/2}")
