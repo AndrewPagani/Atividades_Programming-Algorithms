ce8d4282ece144af86a191a63df6473c00bf2241
Andrew Pagani Lima <andrewpagani.lima0712@gmail.com>
2026-09-07 16:17:49 -0300

    Update 28 - É Possível Formar um Triângulo.py

--- Atividades 16 - 35/28 - É Possível Formar um Triângulo.py
+++ Atividades 16 - 35/28 - É Possível Formar um Triângulo.py
@@ -1,6 +1,11 @@
-l1,l2,l3 = list(map(int, input("Digite os lados: ").split(",")))
+l1, l2, l3 = list(map(int, input("Digite os lados do Triângulo: ").split(",")))
 
 if (l1 + l2 > l3) and (l1 + l3 > l2) and (l2 + l3 > l1):
-    print("FORMAM UM TRIÂNGULO")
+    if l1 == l2 == l3:
+        print("Triângulo Equilátero")
+    elif l1 == l2 or l2 == l3 or l1 == l3:
+        print("Triângulo Isósceles")
+    else:
+        print("Triângulo Escaleno")
 else:
     print("NÃO FORMAM UM TRIÂNGULO")
