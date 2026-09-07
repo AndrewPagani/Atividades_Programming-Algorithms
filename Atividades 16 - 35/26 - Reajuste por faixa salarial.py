ebbbf75856b0100646e4b40207da4524ab045ea5
Andrew Pagani Lima <andrewpagani.lima0712@gmail.com>
2026-09-07 15:47:33 -0300

    Add files via upload

---
+++ Atividades 16 - 35/26 - Reajuste por faixa salarial.py
@@ -0,0 +1,8 @@
+sal = float(input("Digite o salário atual: R$"))
+
+if sal <= 1500:
+    print(f"Novo salário: R${sal*1.15:.2f}\nAumento de 15%")
+elif sal > 1500 and sal <= 3000:
+    print(f"Novo salário: R${sal*1.10:.2f}\nAumento de 10%")
+else:
+        print(f"Novo salário: R${sal*1.05:.2f}\nAumento de 5%")
