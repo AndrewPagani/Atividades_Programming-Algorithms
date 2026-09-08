4a66c813e7a5f36f66409b4f6e28292bbb5fc57d
Andrew Pagani Lima <andrewpagani.lima0712@gmail.com>
2026-09-08 20:12:27 -0300

    Add files via upload

---
+++ Atividades 16 - 35/33 - Dia da Semana.py
@@ -0,0 +1,19 @@
+num = int(input("Insira um número correspondente a algum dia da semana: "))
+
+match num:
+    case 1:
+        print("Domingo")
+    case 2:
+        print("Segunda-Feira")
+    case 3:
+        print("Terça-Feira")
+    case 4:
+        print("Quarta-Feira")
+    case 5:
+        print("Quinta-Feira")
+    case 6:
+        print("Sexta-Feira")
+    case 7:
+        print("Sábado")
+    case default:
+        print("Opção inválida")
